010327523bd22ed46eab6ede5ce778aacc8c42ae
gruntik <gruntov99@mail.ru>
2022-05-03 11:14:57 +0300

    feat: save objects

--- src/main/java/com/example/databasesexample/HQLQuery.java
+++ src/main/java/com/example/databasesexample/HQLQuery.java
@@ -1,11 +1,12 @@
 package com.example.databasesexample;
 
 import com.example.databasesexample.enity.Product;
+import com.example.databasesexample.enity.ProductCategory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class HQLQuery {
@@ -15,15 +16,32 @@ public class HQLQuery {
         Session session = sessionFactory.openSession();
         List<Product> products = null;
         List<Product> productsWithCategory = null;
+        List<Object[]> productsWith = null;
+        List<Product> productsSelect = null;
 
         try {
             session.beginTransaction();
+
+            ProductCategory productCategory = new ProductCategory();
+            productCategory.setId(3L);
+//            productCategory.setTitle("Drink2");
+//            session.save(productCategory);
+
+            Product product = new Product("Vodka2", new BigDecimal(40), productCategory);
+            session.save(product);
 
             Query query = session.createQuery("from Product");
             products = query.list();
 
             productsWithCategory = session.createQuery("from Product p inner join fetch p.category as pc").list();
 
+            Query query1 = session.createQuery("from Product  p inner join p.category as pc with pc.id = :id");
+            query1.setParameter("id", 1L);
+            productsWith = query1.list();
+
+            Query query2 = session.createQuery("select p from Product  p inner join p.category as pc with pc.id = :id");
+            query2.setParameter("id", 1L);
+            productsSelect = query2.list();
 
             session.getTransaction().commit();
         } catch (Exception e) {
@@ -37,6 +55,12 @@ public class HQLQuery {
         System.out.println(products);
 
         System.out.println(productsWithCategory);
+
+        for (Object[] obj : productsWith) {
+            System.out.println(obj[0].toString());
+        }
+
+        System.out.println(productsSelect);
 
     }
 }
